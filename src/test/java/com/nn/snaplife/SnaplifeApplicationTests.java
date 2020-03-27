a22436c2de4a928d4921d39195e2c8c4d5b47549
nikito96 <nikinikolitsov9655@gmail.com>
2020-03-28 00:58:26 +0200

    Init commit

---
+++ src/test/java/com/nn/snaplife/SnaplifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nn.snaplife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnaplifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
